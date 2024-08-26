6a59a5d4b8c03d85edc5da0c7509467f7f3339d1
faisd405 <faisd405@gmail.com>
2024-08-26 11:16:31 +0700

    feat: :tada: Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php include_once 'layout/header.php'; ?>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+    <?php include_once 'layout/navbar.php'; ?>
+    <?php include_once 'layout/sidebar.php'; ?>
+      <div class="main-content">
+        <section class="section">
+            
+        </section>
+      </div>
+      <?php include_once 'layout/footer.php'; ?>
+    </div>
+  </div>
+
+  <?php include_once 'layout/script.php'; ?>
+</body>
+</html>
---
+++ layout/sidebar.php
@@ -0,0 +1,20 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Tabungan Siswa</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">TS</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Home</li>
+            <li><a class="nav-link" href="/"><i class="far fa-square"></i> <span>Dashboard</span></a></li>
+            <li class="menu-header">Datamaster</li>
+            <li><a class="nav-link" href="/students"><i class="far fa-square"></i> <span>Student</span></a></li>
+            <li><a class="nav-link" href="/saving-accounts"><i class="far fa-square"></i> <span>Saving Accounts</span></a></li>
+            <li class="menu-header">Product</li>
+            <li><a class="nav-link" href="/transactions"><i class="far fa-square"></i> <span>Transactions</span></a></li>
+            <li><a class="nav-link" href="/reports"><i class="far fa-square"></i> <span>Reports</span></a></li>
+        </ul>
+    </aside>
+</div>
