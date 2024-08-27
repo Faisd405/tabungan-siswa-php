b58d8b429bb43ba4938d6993f540c2c243a3222f
faisd405 <faisd405@gmail.com>
2024-08-27 21:57:54 +0700

    refactor: Improve error handling and display for form submissions

--- layout/script.php
+++ layout/script.php
@@ -6,11 +6,35 @@
 <script src="/assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
 <script src="/assets/modules/moment.min.js"></script>
 <script src="/assets/js/stisla.js"></script>
-
-<!-- JS Libraies -->
-
-<!-- Page Specific JS File -->
+<script src="/assets/modules/sweetalert/sweetalert.min.js"></script>
 
 <!-- Template JS File -->
 <script src="/assets/js/scripts.js"></script>
 <script src="/assets/js/custom.js"></script>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        <?php if (isset($_SESSION['success'])) { ?>
+            swal({
+                title: "Success",
+                text: `<?php echo $_SESSION['success']; ?>`,
+                icon: "success",
+            });
+
+        <?php
+        }
+        unset($_SESSION['success']);
+        ?>
+
+        <?php if (isset($_SESSION['error'])) { ?>
+            swal({
+                title: "Error",
+                text: `<?php echo $_SESSION['error']; ?>`,
+                icon: "error",
+            });
+        <?php
+        }
+        unset($_SESSION['error']);
+        ?>
+    });
+</script>
